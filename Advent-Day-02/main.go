50841da109a309effd5f47b2adbc217f61090c0b
Witzler <chrisavril24@gmail.com>
2025-12-19 23:53:05 +0100

    Day02 finished

--- Advent-Day-02/main.go
+++ Advent-Day-02/main.go
@@ -1,5 +1,8 @@
 package main
 
+/* Answer for part1: 12599655151 */
+/* Answer for part2: 20942028255*/
+
 //day02 "adventofcode2025day02/utils"
 import (
 	day02 "adventofcode2025day02/utils"
@@ -11,13 +14,11 @@ import (
 // #define
 // global varaibles
 var counter int = 0
+var counter2 int = 0
 
 func main() {
-
-	var counter int
-
 	// Read input file
-	data, err := os.ReadFile("assets/input.txt") //test or input
+	data, err := os.ReadFile("assets/input.txt") // test or input
 	if err != nil {
 		panic(err)
 	}
@@ -27,11 +28,16 @@ func main() {
 
 	for _, pair := range idRange {
 		pair = strings.TrimSpace(pair)
-		numbers := strings.Split(pair, "-")
+		numbers := strings.Split(pair, "-") // Split by hyphen to get individual numbers per pair
 		fmt.Println("First number:", numbers[0], "Second number:", numbers[1])
 
-		counter = counter + day02.FindInvalidIDS(numbers[0], numbers[1])
+		part1, part2 := day02.FindInvalidIDS(numbers[0], numbers[1])
+		counter += part1
+		counter2 += part2
 	}
 
 	fmt.Printf("Total invalid IDs addition: %d\n", counter)
+
+	fmt.Printf("Total invalid IDs addition (part 2): %d\n", counter2)
+
 }
--- Advent-Day-02/utils/day02.go
+++ Advent-Day-02/utils/day02.go
@@ -3,22 +3,24 @@ package utils
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
-func FindInvalidIDS(s1 string, s2 string) int {
+func FindInvalidIDS(s1 string, s2 string) (int, int) {
 
 	var output int = 0
+	var output2 int = 0
 
 	num1, err := strconv.Atoi(s1)
 	if err != nil {
 		fmt.Println("Error converting string to int:", err)
-		return 0
+		return 0, 0 // Return 0 on error
 	}
 
 	num2, err := strconv.Atoi(s2)
 	if err != nil {
 		fmt.Println("Error converting string to int:", err)
-		return 0
+		return 0, 0 // Return 0 on error
 	}
 
 	var index int = num2 - num1
@@ -27,8 +29,13 @@ func FindInvalidIDS(s1 string, s2 string) int {
 	for i := 0; i <= index; i++ {
 		var isInvalid bool = true
 		temptS := strconv.Itoa(temp)
-		if len(temptS)%2 == 0 {
 
+		// checking every number for part 2; accumulate matches instead of overwriting
+		if res := FindInvalidIDS_partTwo(temptS, temp); res != 0 {
+			output2 += res
+		}
+
+		if len(temptS)%2 == 0 {
 			tempLen := len(temptS)
 			var x int = 0
 			var y int = (tempLen / 2)
@@ -48,7 +55,28 @@ func FindInvalidIDS(s1 string, s2 string) int {
 
 	}
 
-	return output
+	return output, output2
+}
+
+/*
+	TODO : Fix function to return the number if it is invalid
+
+It is invalid if it contains the same digits consecutively like 11 1212  111 1414 824824824
+*/
+func FindInvalidIDS_partTwo(s1 string, s1Int int) int {
+	n := len(s1)
+	// Only consider numbers invalid when the entire string is a repetition
+	// of a substring p (p repeated >= 2 times). Examples: "111", "1010", "824824824".
+	for l := 1; l <= n/2; l++ {
+		if n%l != 0 {
+			continue
+		}
+		if strings.Repeat(s1[0:l], n/l) == s1 {
+			return s1Int
+		}
+	}
+
+	return 0
 }
 
 func ExampleFunction() {
